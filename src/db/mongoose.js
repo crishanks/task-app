1a7180298fcc72dbe4213b768b112ec7d94f842c
github username <crishanks12@gmail.com>
2019-08-29 17:41:58 -0600

    customized validators with validator and mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -7,16 +8,35 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number')
+      }
+    }
   }
 })
 
 const me = new User({
-  name: 'Cris',
-  age: 27
+  name: 'Steve  ',
+  email: '  myemail@yahoo.io  '
 })
 
 me.save().then(() => {
@@ -25,22 +45,22 @@ me.save().then(() => {
   console.log('Error', error)
 })
 
-const Task = mongoose.model('Task', {
-  description: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  }
-})
+// const Task = mongoose.model('Task', {
+//   description: {
+//     type: String
+//   },
+//   completed: {
+//     type: Boolean
+//   }
+// })
 
-const dishes = new Task({
-  description: 'Wash the dishes',
-  completed: true
-})
+// const dishes = new Task({
+//   description: 'Wash the dishes',
+//   completed: true
+// })
 
-dishes.save().then(() => {
-  console.log(dishes)
-}).catch((error) => {
-  console.log('Error', error)
-})
+// dishes.save().then(() => {
+//   console.log(dishes)
+// }).catch((error) => {
+//   console.log('Error', error)
+// })
