47bcfaf8c014ea34c0e3bd2b55be78204646f5e3
github username <crishanks12@gmail.com>
2019-08-29 17:52:03 -0600

    custom validations for tasks with mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -31,36 +31,50 @@ const User = mongoose.model('User', {
         throw new Error('Age must be a positive number')
       }
     }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"')
+      }
+    }
   }
 })
 
-const me = new User({
-  name: 'Steve  ',
-  email: '  myemail@yahoo.io  '
-})
-
-me.save().then(() => {
-  console.log(me)
-}).catch((error) => {
-  console.log('Error', error)
-})
-
-// const Task = mongoose.model('Task', {
-//   description: {
-//     type: String
-//   },
-//   completed: {
-//     type: Boolean
-//   }
+// const me = new User({
+//   name: 'Steve  ',
+//   email: '  myemail@yahoo.io  ',
+//   password: 'ssword123'
 // })
 
-// const dishes = new Task({
-//   description: 'Wash the dishes',
-//   completed: true
-// })
-
-// dishes.save().then(() => {
-//   console.log(dishes)
+// me.save().then(() => {
+//   console.log(me)
 // }).catch((error) => {
 //   console.log('Error', error)
 // })
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const dishes = new Task({
+  description: '  Wash the dishes    '
+})
+
+dishes.save().then(() => {
+  console.log(dishes)
+}).catch((error) => {
+  console.log('Error', error)
+})
