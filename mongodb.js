4b8fc3334b5aa15d4aa14ce81309d19dfa09d7c1
github username <crishanks12@gmail.com>
2019-08-28 23:14:45 -0600

    update collections with updateOne and updateMany

--- mongodb.js
+++ mongodb.js
@@ -5,33 +5,35 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
   if (error) {
     return console.log('Unable to connect to the database!')
   }
   
   const db = client.db(databaseName)
 
-  // db.collection('users').findOne({ _id: new ObjectID("5d67440629ae591bbec1fdbd") }, (error, user) => {
-  //   if (error) {
-  //     return console.log('Could not fetch.')
+  // db.collection('users').updateOne({
+  //   _id: new ObjectID("5d67428f1e9b191b9528ed09")
+  // }, {
+  //   $inc: {
+  //     age: 1
   //   }
-  //   console.log(user)
+  // }).then((result) => {
+  //   console.log(result)
+  // }).catch((error) => {
+  //   console.log(error)
   // })
 
-  // db.collection('users').find({ age: 27 }).toArray((error, users) => {
-  //   console.log(users)
-  // })
-
-  // db.collection('users').find({ age: 27 }).count((error, count) => {
-  //   console.log(count)
-  // })
-
-  db.collection('tasks').findOne({ _id: new ObjectID("5d6744f7bfc6431beb9c40cb") }, (error, task) => {
-    console.log(task)
+  db.collection('tasks').updateMany({
+    completed: false
+  }, {
+    $set: {
+      completed: true
+    }
+  }).then((result) => {
+    console.log(result)
+  }).catch((error) => {
+    console.log(error)
   })
 
-  db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-    console.log(tasks)
-  })
 })
