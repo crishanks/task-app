1a82550ebbfe83f9606d62349e0c768bca109235
github username <crishanks12@gmail.com>
2019-08-31 22:06:03 -0600

    asyn await practice in playground. patch requests to update user and task

---
+++ playground/promise-chaining-2.js
@@ -0,0 +1,24 @@
+require('../src/db/mongoose')
+const Task = require('../src/models/task')
+
+// Task.findByIdAndDelete('5d6aae671acf59234bfc36d3').then((task) => {
+//   console.log(task)
+//   return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//   console.log(result)
+// }).catch(e => {
+//   console.log(e)
+// })
+
+
+const deleteTaskAndCount = async (id) => {
+  const task = await Task.findByIdAndDelete(id)
+  const count = await Task.countDocuments({ completed: false })
+  return count
+}
+
+deleteTaskAndCount("5d6aaf6db413c72374d2b40b").then(count => {
+  console.log(count)
+}).catch(e => {
+  console.log(e)
+})
