fe76a4141d558e73518c8fdedfcf09385d959abe
github username <crishanks12@gmail.com>
2019-09-02 13:01:56 -0600

    add middleware for all requests

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -39,10 +40,26 @@ const userSchema = new mongoose.Schema({
         throw new Error('Password cannot contain "password"')
       }
     }
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
 // Authentication
+userSchema.methods.generateAuthToken = async function() {
+  const user = this
+  const token = jwt.sign({ _id: user._id.toString() }, 'taskappsecret')
+
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+  
+  return token
+}
+
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email })
 
