9b5a1249bc34a2f977ef048d7cf47341b54ebd71
github username <crishanks12@gmail.com>
2019-09-02 21:13:52 -0600

    pagination and filtered searching by query for GET /tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,10 +18,33 @@ router.post('/tasks', auth, async (req, res) => {
   }
 })
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt:asc
 router.get('/tasks', auth, async (req, res) => {
+  const match = {}
+  const sort = {}
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true'
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':')
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+  }
+
   try {
-    const tasks = await Task.find({ owner: req.user._id})
-    res.send(tasks)
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort
+      }
+    }).execPopulate()
+    res.send(req.user.tasks)
   } catch (e) {
     res.status(500).send()
   }
@@ -64,7 +87,7 @@ router.delete('/tasks/:id', auth, async (req, res) => {
     const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
 
     if (!task) return res.status(404).send()
-    
+
     res.send(task)
   } catch (e) {
     res.status(500).send(e)
