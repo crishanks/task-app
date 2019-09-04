98bcb42892da906277a1b8b6724ae06e35e8e8c3
github username <crishanks12@gmail.com>
2019-09-03 19:19:43 -0600

    ready to deploy

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,8 +1,6 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendgridAPIKey = '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'
-
-sgMail.setApiKey(sendgridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
   sgMail.send({
