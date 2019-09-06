a5116e58d2626fba7019af2abbac6c9177bae062
github username <crishanks12@gmail.com>
2019-09-05 23:58:02 -0600

    create db tests and users for more diverse testing. initial task testing

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,20 +1,51 @@
 const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
-const { userOneId, userOne, populateDatabase } = require('./fixtures/db')
+const { 
+    userOneId, 
+    userOne,
+    userTwoId, 
+    userTwo, 
+    taskOne,
+    taskTwo,
+    taskThree,
+    populateDatabase 
+} = require('./fixtures/db')
 
 beforeEach(populateDatabase)
 
 test('Should create task for user', async () => {
     const response = await request(app)
-    .post('/tasks')
-    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
-    .send({
-        description: 'From my test'
-    })
-    .expect(201)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'From my test'
+        })
+        .expect(201)
 
     const task = await Task.findById(response.body._id)
     expect(task).not.toBeNull()
     expect(task.completed).toEqual(false)
+})
+
+test('Should get all tasks for a user', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body.length).toEqual(2)
+
+})
+
+test('User should not be able to delete another users tasks', async () => {
+    await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+        
+    const task = Task.findById(taskOne.id)
+    expect(task).not.toBeNull()
 })
